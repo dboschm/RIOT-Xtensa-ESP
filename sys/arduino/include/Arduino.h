a367193058c9902d7daa3668eaa2a3102affed4c
Gunar Schorcht <gunar@schorcht.net>
2018-12-11 21:31:11 +0100

    sys/arduino: add Arduino.h for compatibility
    Arduino libraries often include Arduino.h. For source code compatibility this header file is required. Header guards in file arduino.hpp had to be renamed.

---
+++ sys/arduino/include/Arduino.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2018 Gunar Schorcht
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     sys_arduino
+ * @brief       Wrapper to keep source code compatibility for Arduino.h
+ * @author      Gunar Schorcht <gunar@schorcht.net>
+ * @file
+ * @{
+ */
+
+#ifndef ARDUINO_H
+#define ARDUINO_H
+
+#include "arduino.hpp"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ARDUINO_H */
+/** @} */
